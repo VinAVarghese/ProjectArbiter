0be8cc39432cd862c0354b1b18599b74611f38cc
Arati Bhandwalkar <arativbhandwalkar@gmail.com>
2020-07-23 13:10:13 -0700

    zomato edit

--- public/assets/js/zomato.js
+++ public/assets/js/zomato.js
@@ -1,81 +1,71 @@
-// Listeners for click &/or keypress to start search/AJAX call
-// $("#search").click(() => {
-//     const type = $("#category").val().trim();
-//     const q = $("#queryBar").val()
-//     tasteDiveApi(type, q)
-// })
+// Form listener grabbing/storing values
+$("#search-form").on("submit", (event) => {
+    event.preventDefault()
+    const type = $("#category").val();
+    const q = $("#queryBar").val()
+    searchZomato(type, q)
+})
 
-// $("#queryBar").keydown(() => {
-//     if (event.which == 13) {
-//         const type = $("#category").val().trim();
-//         const q = $("#queryBar").val()
-//         tasteDiveApi(type, q)
-//     }
-// })
+// Need to send "type" and "q" data server-side
+const searchZomato = (type, q) => {
+    const searchData = {
+        type,
+        q
+    }
+    $.ajax("/api/zomato", {
+        type: "POST",
+        data: searchData,
+    })
+}
 
+// Need to recieve "option" from server-side to render card
+const renderTasteDiveOption = (data) => {
+    // queryBar validator
+    if (data.similar.info[0].type == "unknown") {
+        $("#queryBar").val("Not a valid search, please try again")
+        $("#queryBar").attr("class", "error");
+    } else
+        $("#queryBar").attr("class", "errorFix");
+    // Displaying random option details in a card
+    const optionCard = $("<div>");
+    optionCard.attr("class", "card");
+    $("#centralDiv").append(optionCard);
 
-// // Function: AJAX call to TasteDive
-// const tasteDiveApi = (type, q) => {
-//     const apiKey = "e67072ffec13f1c95781c11c4b394576"
-//     const queryURL = `https://zomato.com/api/similar?q=${type}:${q}&info=1&limit=100&k=${apiKey}`
+    const optionHeader = $("<div>");
+    optionHeader.attr("class", "card-divider");
+    optionHeader.text(option.name);
+    $(optionCard).append(optionHeader);
 
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then((data) => {
-//         if (err) throw err
-//         console.log(data);
-//         // queryBar validator 
-//         if (data.similar.info[0].type == "unknown") {
-//             $("#queryBar").val("Not a valid search, please try again")
-//             $("#queryBar").attr("class", "error");
-//         } else
-//             $("#queryBar").attr("class", "errorFix");
-//         // Selecting 1 random option/100 responses to display
-//         const randomNum = Math.floor(Math.random() * 101);
-//         const option = data.similar.results[randomNum]
-//         // Displaying random option details in a card
-//         const optionCard = $("<div>");
-//         optionCard.attr("class", "card");
-//         $("#centralDiv").append(optionCard);
+    const optionSection = $("<div>");
+    optionSection.attr("class", "card-section");
+    $(optionCard).append(optionSection);
 
-//         const optionHeader = $("<div>");
-//         optionHeader.attr("class", "card-divider");
-//         optionHeader.text(option.name);
-//         $(optionCard).append(optionHeader);
+    if (option.wTeaser !== null) {
+        const h4About = $("<h4>");
+        h4About.text("About")
+        $(optionSection).append(h4About);
+        const pAbout = $("<p>");
+        pAbout.text(option.wTeaser)
+        $(optionSection).append(pAbout);
+    }
 
-//         const optionSection = $("<div>");
-//         optionSection.attr("class", "card-section");
-//         $(optionCard).append(optionSection);
+    if (option.wUrl !== null) {
+        const h4Links = $("<h4>");
+        h4Links.text("Links")
+        $(optionSection).append(h4Links);
+        const aWikiTag = $("<a>");
+        aWikiTag.attr("href", option.wUrl)
+        aWikiTag.text("Wikipedia")
+        $(optionSection).append(aWikiTag);
+    }
 
-//         if (option.wTeaser !== null){
-//             const h4About = $("<h4>");
-//             h4About.text("About")
-//             $(optionSection).append(h4About);
-//             const pAbout = $("<p>");
-//             pAbout.text(option.wTeaser)
-//             $(optionSection).append(pAbout);
-//         }
-        
-//         if (option.wUrl !== null){
-//             const h4Links = $("<h4>");
-//             h4Links.text("Links")
-//             $(optionSection).append(h4Links);
-//             const aWikiTag = $("<a>");
-//             aWikiTag.attr("href", option.wUrl)
-//             aWikiTag.text("Wikipedia")
-//             $(optionSection).append(aWikiTag);
-//         }
-
-//         if (option.yUrl !== null){
-//             const h4YTube = $("<h4>");
-//             h4YTube.text("YouTube")
-//             $(optionSection).append(h4YTube);
-//             const aWikiTag = $("<a>");
-//             aWikiTag.attr("href", option.yUrl)
-//             aWikiTag.text("Preview")
-//             $(optionSection).append(aWikiTag);
-//         }
-//     })
-// }
-
+    if (option.yUrl !== null) {
+        const h4YTube = $("<h4>");
+        h4YTube.text("YouTube")
+        $(optionSection).append(h4YTube);
+        const aWikiTag = $("<a>");
+        aWikiTag.attr("href", option.yUrl)
+        aWikiTag.text("Preview")
+        $(optionSection).append(aWikiTag);
+    }
+}
